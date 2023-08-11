1e58dd103d9892a1baf977c6597715ea7213f6e0
cjdrago <crdrago@alumnos.uai.cl>
2023-08-11 15:12:16 -0400

    second commit

---
+++ my-app/src/app/page.js
@@ -0,0 +1,12 @@
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className="h-screen flex justify-center items-center font-mono">
+      <div>
+        Hello world! <br></br>
+        From Cristóbal Drago :)
+      </div>
+    </div>
+  )
+}
